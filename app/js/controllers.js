3d71bf9ce2cf78ae9512000a85e1a625cf22e57c
Phillip Wills <philwills@gmail.com>
2013-11-27 09:30:57 -0800

    Adding functionality to import menu from json service

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -21,9 +21,35 @@
         };
     });
 
-    menuEditor.controller('menuEditorCtrl', function menuEditorCtrl($scope) {
+    menuEditor.controller('menuEditorCtrl', function menuEditorCtrl($scope, $http) {
         $scope.roots = [];
 
+        $scope.getMenu = function() {
+            if (confirm("You will lose any unsaved work, are you sure?")) {
+                $scope.roots = [];
+
+                $http({method: 'GET', url: $scope.menuGetAddress}).
+                    success(function(data, status, headers, config) {
+                        var menu = new TreeNode.ImportFromJSON({
+                            data: data,
+                            childKey: 'Children'
+                        });
+
+                        $scope.roots.push(menu);
+                    }).
+                    error(function(data, status, headers, config) {
+                        /*jshint devel: true*/
+                        console.log("error");
+                        console.dir(data);
+                        console.dir(status);
+                    });
+            }
+        };
+
+        $scope.putMenu = function() {
+            var json = JSON.stringify($scope.roots[0]);
+        };
+                        
         $scope.add = function(item) {
             if (item) {
                 item.add(new TreeNode());
