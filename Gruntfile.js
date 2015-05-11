adc161c62f2e048ce7349378475c43fd55fa81ad
Phillip Wills <philwills@gmail.com>
2015-05-10 21:49:27 -0700

    Adding a large input file and connect / watch

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,10 @@
-/*global module*/
+/* global require, module */
+/* jshint camelcase: false */
+var LIVERELOAD_PORT = 35729;
+var lrSnippet = require('connect-livereload')({port: LIVERELOAD_PORT});
+var mountFolder = function (connect, dir) {
+    return connect.static(require('path').resolve(dir));
+};
 
 module.exports = function(grunt) {
 
@@ -22,13 +28,58 @@ module.exports = function(grunt) {
             build: {
                 src: ['app/css/*.css']
             }
+        },
+        connect: {
+            options: {
+                port: 9000
+            },
+            livereload: {
+                options: {
+                    middleware: function (connect) {
+                        return [
+                            lrSnippet,
+                            mountFolder(connect, 'app/')
+                        ];
+                    }
+                }
+            }
+        },
+        watch: {
+            gruntfile: {
+                files: '<%= jshint.gruntfile.src %>',
+                tasks: ['jshint:gruntfile']
+            },
+            less: {
+                files: 'less/{,*/}*.less',
+                tasks: ['less']
+            },
+            livereload: {
+                options: {
+                    livereload: LIVERELOAD_PORT
+                },
+                files: [
+                    'app/*.html',
+                    'app/css/{,*/}*.css',
+                    'app/js/{,*/}*.js',
+                    'app/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
+                ]
+            }
+        },
+        open: {
+            build: {
+                path: 'http://localhost:9000'
+            }
         }
     });
 
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-open');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-csslint');
     grunt.loadNpmTasks('grunt-contrib-less');
 
     grunt.registerTask('default', ['jshint', 'less:build', 'csslint']);
+    grunt.registerTask('serve', ['default', 'connect', 'open', 'watch']);
 };
 
